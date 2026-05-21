26d19389e66a107894cfb85069dcd4731c91a26c
אופק סער <ofeksa@mta.ac.il>
2026-05-21 11:40:14 +0300

    refactor: deleting ui lib and

--- ui/electron/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  closeWindow: () => ipcRenderer.send('window-close'),
-  minimizeWindow: () => ipcRenderer.send('window-minimize'),
-});
